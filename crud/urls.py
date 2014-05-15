1508c9cc98eb07651482a580f6643403e0a156b4
Joe21 <joej21@gmail.com>
2014-05-14 23:02:02 -0400

    index working, link not passing the id number

--- crud/urls.py
+++ crud/urls.py
@@ -1,1 +1,14 @@
-urls.py
+from django.conf.urls import patterns, url
+
+from crud import views
+
+urlpatterns = patterns('',
+	# ex: /crud/
+	url(r'^$', views.index, name='index'),
+
+	# ex: /crud/5/detail/
+	url(r'^(?P<athlete_id>\d+)/$', views.detail, name='detail'),
+
+	# ex: /crud/add/
+	# url(r'^(?P<athlete_id>/d+)/add/$'), views.add, name'add'),
+)
--- crud/views.py
+++ crud/views.py
@@ -1,4 +1,18 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from crud.models import Athlete
 
 def index(request):
-	return HttpResponse("Welcome to the index page")
+	all_athletes = Athlete.objects.all()
+	template = loader.get_template('crud/index.html')
+	context = RequestContext(request, {
+		'all_athletes' : all_athletes,
+		})
+	return HttpResponse(template.render(context))
+
+def detail(request, athlete_id):
+	return HttpResponse("Welcome to the detail page")
+
+def add(request):
+	return HttpResponse("Welcome to the add page")
