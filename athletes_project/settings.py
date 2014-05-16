11c263d0c5afd14f12c3672a821b3bf583b25287
Joe21 <joej21@gmail.com>
2014-05-15 20:44:10 -0400

    testing bootstrap import

--- athletes_project/settings.py
+++ athletes_project/settings.py
@@ -126,6 +126,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'crud',
+    'bootstrap3',
 )
 
 # A sample logging configuration. The only tangible logging
