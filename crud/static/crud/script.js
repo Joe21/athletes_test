3584ab7d6c1720ad8214ed89e433b608833acdae
Joe21 <joej21@gmail.com>
2014-05-16 01:00:03 -0400

    still styling

--- crud/static/crud/script.js
+++ crud/static/crud/script.js
@@ -1,8 +1,28 @@
-function backgroundChange() {
+// button functionality
+function meME(){
+	var el = $('#music-container');
+	var player = $('#audio-player');
 	var body = $('body');
-	if(body.css('background-color')==='rgb(255, 255, 255)'){
-		$('body').css('background-color', '#000000');
+
+	if(stealth === true){
+		el.append("<audio id='audio-player' src='https://s3.amazonaws.com/samplepublicbucket/me-unchartered_worlds_cutup.mp3' controls autoplay>");
+		$('body').css("background-image", "url('/static/crud/n7.jpg')");
+		stealth = false;
 	} else {
-		$('body').css('background-color', '#ffffff');
+		el.empty();
+		$('body').css("background-image", "url('/static/crud/football.jpg')");
+		stealth = true;
 	}
 }
+
+// {% static 'crud/style.css' %}
+// 	background-image: url('background.jpg');
+
+
+// Global variable
+var stealth = true;
+
+// On window load
+$(function(){
+	$('#crazy-button').on('click', meME);
+});
