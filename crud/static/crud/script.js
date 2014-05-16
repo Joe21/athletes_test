53d6f873fc847f47990b52793b7cc4aab0b56bd4
Joe21 <joej21@gmail.com>
2014-05-16 02:09:20 -0400

    done styling, proceed to begin deployment

--- crud/static/crud/script.js
+++ crud/static/crud/script.js
@@ -3,21 +3,39 @@ function meME(){
 	var el = $('#music-container');
 	var player = $('#audio-player');
 	var body = $('body');
+	var block = $('.block-container');
 
 	if(stealth === true){
 		el.append("<audio id='audio-player' src='https://s3.amazonaws.com/samplepublicbucket/me-unchartered_worlds_cutup.mp3' controls autoplay>");
+		block.hide();
 		$('body').css("background-image", "url('/static/crud/n7.jpg')");
 		stealth = false;
 	} else {
 		el.empty();
+		block.show();
 		$('body').css("background-image", "url('/static/crud/football.jpg')");
 		stealth = true;
 	}
 }
 
-// {% static 'crud/style.css' %}
-// 	background-image: url('background.jpg');
+function dorkyglow(){
+	var option = {
+		x: 0,
+		y: 0,
+		radius: 10,
+		color: 'red'
+	};
+	$('h2').textShadow(option);
+}
 
+
+	// setInterval(function(){
+	// 	$('a').css('text-shadow: 0 0 10px red;');
+	// }, 1000);
+
+// function glow(){
+// 	this.css('text-shadow: 0 0 10px yellow;');
+// }
 
 // Global variable
 var stealth = true;
